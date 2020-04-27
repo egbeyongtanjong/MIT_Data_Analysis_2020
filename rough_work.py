9b1273196de326dd6b27f828da9c01d454f1c374
Egbeyong Tanjong <etanjo1@lsu.edu>
2020-04-27 13:57:14 -0500

    more thoughts added

--- rough_work.py
+++ rough_work.py
@@ -155,9 +155,105 @@ for i in range(3):
     print (f(i))
 
 """
-
+"""
+#showing what happens to dictionaries with dupicated keys
 hand= {'d':2,'o':1,'g':3,'d':2}
 print(hand['d'])
+"""
+
+"""
+alphabet = {}
+shift = 3
+
+for i in range (65,91):
+    alphabet[chr(i)] = chr(i)
+
+alphabet[' ']=' '
+
+for i in range (97,123):
+    alphabet[chr(i)] = chr(i)
+print(alphabet)
+
+if shift == 0:
+    print(alphabet)
+
+elif shift > 0:
+    temp = alphabet.copy()
+    for key in alphabet.keys():
+        if key != ' ' and ord(key) <= 90 - shift:
+            temp[key] = alphabet[chr(shift+ord(key))]
+        elif key != ' ' and ord(key) <= 90:
+            val = shift+ord(key)
+            if val == 91:
+                temp[key] = ' '
+            else:
+                temp[key] = alphabet[chr(val%90 + 63)]
+        elif key != ' ' and ord(key) <= 122 - shift:
+            temp[key] = alphabet[chr(shift+ord(key))]
+        elif key != ' ' and ord(key) <= 122:
+            val = shift+ord(key)
+            if val == 123:
+                temp[key] = ' '
+            else:
+                temp[key] = alphabet[chr(val%122 + 95)]
+        else:
+            temp[key] = alphabet[chr(97+shift-1)]
+
+else:
+    temp = alphabet.copy()
+    for key in alphabet.keys():
+        if key != ' ' and ord(key) - abs(shift) >= 97:
+            temp[key] = alphabet[chr(ord(key)-abs(shift))]
+        elif key != ' ' and ord(key) > 96 and ord(key) - abs(shift) > 70:
+            val = ord(key) - abs(shift)
+            if val == 96:
+                temp[key] = ' '
+            else:
+                temp[key] = alphabet[chr(val + 27)]
+        elif key != ' ' and ord(key) - abs(shift) >= 65:
+            temp[key] = alphabet[chr(ord(key)-abs(shift))]
+        elif key != ' ' and ord(key) - abs(shift) > 38:
+            val = ord(key) - abs(shift)
+            if val == 64:
+                temp[key] = ' '
+            else:
+                print(val)
+                temp[key] = alphabet[chr(val + 27)]
+        else:
+            temp[key] = alphabet[chr(90 - abs(shift) + 1)]
+    
+    
+    
+    
+    
+text = "The goal is to become good"
+text = "QebXdlyiXfpXqlXzb ljbXdlla"   
+print(temp)
+
+newStr = ' '
+ref = temp
+for c in text:
+    if c in ref.keys():
+        newStr += ref[c]
+    else:
+        newStr += c
+    #return newStr
+
+print(newStr)
+"""
+
+s = "Good morning"
+s = s[0:2] + s[5]
+print(s)
+
+
+start_positions = [0]
+s = "The boy is good"
+for i in range(len(s)):
+    if s[i] == ' ':
+        start_positions.append(i+1)
+print(start_positions)
+
 
 
 
