e028ac22a6a4f6bec1a153f4b5d2bbf09150d9ed
Egbeyong Tanjong <etanjo1@lsu.edu>
2020-05-07 18:17:05 -0500

    some more notes

--- Lecture_notes/Lecture12.py
+++ Lecture_notes/Lecture12.py
@@ -197,14 +197,14 @@ def simWalks(numSteps, numTrials):
     homer = Drunk('Homer')     
     origin = Location(0, 0)     
     distances = []    
-    for t in range(numTrials):       
+    for t in range(numSteps):       
         f = Field()        
         f.addDrunk(homer, origin)        
         distances.append(walk(f, homer, numTrials))     
     return distances 
  
 def drunkTest(numTrials):     
-    for numSteps in [10, 100, 1000, 10000, 100000]:      
+    for numSteps in [0, 10, 100, 1000, 10000, 100000]:      
         distances = simWalks(numSteps, numTrials)      
         print ('Random walk of ' + str(numSteps) + ' steps' )   
         print ('  Mean =', sum(distances)/len(distances)   )   
